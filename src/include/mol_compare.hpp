f2e1a953573fde343ce17d01df55888124783aaa
bodowd <odowd.bing@gmail.com>
2024-07-15 20:49:23 +0200

    Revert "wip-bind"
    This reverts commit 2d8eae9120b56269795f79060e79d10ae3c9d7b5.

--- src/include/mol_compare.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "common.hpp"
-namespace duckdb_rdkit {
-void RegisterCompareFunctions(DatabaseInstance &instance);
-} // namespace duckdb_rdkit
--- src/include/types.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "common.hpp"
-
-namespace duckdb_rdkit {
-
-LogicalType Mol();
-void RegisterTypes(DatabaseInstance &instance);
-
-} // namespace duckdb_rdkit
