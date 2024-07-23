23e89c9fc6c9d31ce01ff67194334d7f9d9427d4
bodowd <odowd.bing@gmail.com>
2024-07-23 23:13:54 +0200

    add sql statements for experiments

--- demo.sql
+++ demo.sql
@@ -25,5 +25,62 @@ select * from molecule where umbra_is_exact_match(umbra_mol,'CCC');
 
 
 
+-- copy data from pg into duckdb database using these statements, but just change the table names
+copy (select * from pg.binding_sites) to 'binding_sites.parquet' (format parquet);
+create table binding_sites as select * from 'binding_sites.parquet';
 
 
+
+
+
+-- joining three tables with an exact match search
+
+SELECT pbd.prediction_method, a.value, a.relation, m.mol FROM molecule m
+    INNER JOIN activities a ON a.molregno=m.id
+    INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+    INNER JOIN compound_properties cp ON cp.molregno=m.id
+    WHERE is_exact_match(m.mol, 'COc1cc(/C=C/C(=O)OCCCCCCN(C)CCCCOC(=O)c2c3ccccc3cc3ccccc23)cc(OC)c1OC');
+
+SELECT pbd.prediction_method, a.value, a.relation, m.umbra_mol FROM molecule m
+    INNER JOIN activities a ON a.molregno=m.id
+    INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+    INNER JOIN compound_properties cp ON cp.molregno=m.id
+    WHERE umbra_is_exact_match(m.umbra_mol, 'COc1cc(/C=C/C(=O)OCCCCCCN(C)CCCCOC(=O)c2c3ccccc3cc3ccccc23)cc(OC)c1OC')
+    ;
+
+
+
+
+
+SELECT pbd.prediction_method, a.value, a.relation, m.mol FROM molecule m
+  INNER JOIN activities a ON a.molregno=m.id
+  INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+  INNER JOIN compound_properties cp ON cp.molregno=m.id
+  WHERE is_exact_match(m.mol, 'CC(=O)Nc1nnc(S(N)(=O)=O)s1');
+
+-- with umbra mol
+SELECT pbd.prediction_method, a.value, a.relation, m.umbra_mol FROM molecule m
+  INNER JOIN activities a ON a.molregno=m.id
+  INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+  INNER JOIN compound_properties cp ON cp.molregno=m.id
+  WHERE umbra_is_exact_match(m.umbra_mol, 'CC(=O)Nc1nnc(S(N)(=O)=O)s1')
+  ;
+
+
+
+
+-- standard mol
+SELECT avg(a.value), count(a.value), a.relation, m.mol FROM molecule m
+INNER JOIN activities a ON a.molregno=m.id
+INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+INNER JOIN compound_properties cp ON cp.molregno=m.id
+WHERE is_exact_match(m.mol, 'CC(=O)Nc1nnc(S(N)(=O)=O)s1')
+GROUP BY m.mol, a.relation;
+
+-- now with umbra mol
+SELECT avg(a.value), count(a.value), a.relation, m.umbra_mol FROM molecule m
+INNER JOIN activities a ON a.molregno=m.id
+INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+INNER JOIN compound_properties cp ON cp.molregno=m.id
+WHERE umbra_is_exact_match(m.umbra_mol, 'CC(=O)Nc1nnc(S(N)(=O)=O)s1')
+GROUP BY m.umbra_mol, a.relation;
