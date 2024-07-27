e4eb50cf662f83cbc82e0e5f806a8191f06b96b9
bodowd <odowd.bing@gmail.com>
2024-07-27 18:51:35 +0200

    7 byte header

--- src/include/types.hpp
+++ src/include/types.hpp
@@ -13,35 +13,54 @@ void RegisterTypes(DatabaseInstance &instance);
 struct umbra_mol_t {
 
 public:
-  static constexpr idx_t NUM_ATOMS_BYTES = 2;
-  static constexpr idx_t NUM_BONDS_BYTES = 2;
+  static constexpr idx_t NUM_ATOMS_BYTES = 1;
+  static constexpr idx_t NUM_BONDS_BYTES = 1;
   static constexpr idx_t AMW_BYTES = 2;
-  static constexpr idx_t NUM_RINGS_BYTES = 2;
+  static constexpr idx_t NUM_RINGS_BYTES = 1;
   static constexpr idx_t BMOL_SIZE_BYTES = 2;
   static constexpr idx_t HEADER_SIZE = NUM_ATOMS_BYTES + NUM_BONDS_BYTES +
                                        AMW_BYTES + NUM_RINGS_BYTES +
                                        BMOL_SIZE_BYTES;
   static constexpr idx_t UINT16_MAX_SIZE = NumericLimits<uint16_t>::Maximum();
-  uint16_t num_atoms;
-  uint16_t num_bonds;
+  static constexpr idx_t UINT8_MAX_SIZE = NumericLimits<uint8_t>::Maximum();
+
+  // Experimented with 1 byte values 7 byte header
+  // 2 bytes also works fine to give 10 byte header
+  uint8_t num_atoms;
+  uint8_t num_bonds;
   uint16_t amw;
-  uint16_t num_rings;
+  uint8_t num_rings;
   uint16_t bmol_size;
   std::string bmol;
 
   // default constructor for deserialization
   umbra_mol_t() = default;
 
-  umbra_mol_t(uint16_t num_atoms, uint16_t num_bonds, uint16_t amw,
-              uint16_t num_rings, const std::string &binary_mol)
+  umbra_mol_t(uint8_t num_atoms, uint8_t num_bonds, uint16_t amw,
+              uint8_t num_rings, const std::string &binary_mol)
       : num_atoms(num_atoms), num_bonds(num_bonds), amw(amw),
         num_rings(num_rings), bmol_size(binary_mol.size()), bmol(binary_mol) {
 
-    if (num_atoms > UINT16_MAX_SIZE || num_bonds > UINT16_MAX_SIZE ||
-        amw > UINT16_MAX_SIZE || num_rings > UINT16_MAX_SIZE) {
+    if (num_atoms > UINT8_MAX_SIZE) {
+      num_atoms = UINT8_MAX_SIZE;
+    }
+
+    if (num_bonds > UINT8_MAX_SIZE) {
+      num_bonds = UINT8_MAX_SIZE;
+    }
+
+    if (amw > UINT16_MAX_SIZE) {
+      amw = UINT16_MAX_SIZE;
+    }
+
+    if (num_rings > UINT8_MAX_SIZE) {
+      num_rings = UINT8_MAX_SIZE;
+    }
+
+    if (bmol_size > UINT16_MAX_SIZE) {
       throw OutOfRangeException(
-          "Cannot support a molecule of this size. There are properties of "
-          "this molecule larger than the supported size: '%d'",
+          "Cannot support a molecule of this size."
+          "this molecule object is larger than the max supported size: '%d'",
           UINT16_MAX_SIZE);
     }
   }
