7c4fbb400942d6ea29c96c6f61a13f79b9a5d785
bodowd <odowd.bing@gmail.com>
2024-07-31 21:49:25 +0200

    record queries to be run and fix table name in pg query

--- umbra_exact_match.sql
+++ umbra_exact_match.sql
@@ -1,16 +1,19 @@
 .timer on
 .open chembl_33.duckdb
 
+--Query1
+select molregno,canonical_smiles, rdkit_mol, umbra_mol from molecule where umbra_is_exact_match(umbra_mol,'Cc1cn([C@H]2C[C@H](N=[N+]=[N-])[C@@H](CO)O2)c(=O)[nH]c1=O');
+--Query2
 
-select molregno,canonical_smiles, mol, umbra_mol from molecule where umbra_is_exact_match(umbra_mol,'Cc2cn([C@H]2C[C@H](N=[N+]=[N-])[C@@H](CO)O2)c(=O)[nH]c1=O');
-
-select molregno,canonical_smiles, mol, umbra_mol from molecule where umbra_is_exact_match(umbra_mol,'CCC');
+select molregno,canonical_smiles, rdkit_mol, umbra_mol from molecule where umbra_is_exact_match(umbra_mol,'CCC');
+--Query3
 
 SELECT pbd.prediction_method, a.value, a.relation, m.umbra_mol FROM molecule m
   INNER JOIN activities a ON a.molregno=m.molregno
   INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
   INNER JOIN compound_properties cp ON cp.molregno=m.molregno
   WHERE umbra_is_exact_match(m.umbra_mol, 'COc1cc(/C=C/C(=O)OCCCCCCN(C)CCCCOC(=O)c2c3ccccc3cc3ccccc23)cc(OC)c1OC');
+--Query4
 
 SELECT avg(a.value), count(a.value), a.relation, m.umbra_mol FROM molecule m
   INNER JOIN activities a ON a.molregno=m.molregno
