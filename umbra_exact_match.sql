517fd2e3f0bef6d7ad58e7d0a7d0f57520983e38
bodowd <odowd.bing@gmail.com>
2024-07-31 16:14:29 +0200

    add umbra exact match queries

---
+++ umbra_exact_match.sql
@@ -0,0 +1,51 @@
+.timer on
+.open chembl_33.duckdb
+
+alter table molecule drop column umbra_mol;
+alter table molecule add column umbra_mol UmbraMol;
+update molecule set umbra_mol=umbra_mol_from_smiles(canonical_smiles);
+
+
+select molregno,canonical_smiles, rdkit_mol, umbra_mol from molecule where umbra_is_exact_match(umbra_mol,'Cc1cn([C@H]2C[C@H](N=[N+]=[N-])[C@@H](CO)O2)c(=O)[nH]c1=O');
+
+select molregno,canonical_smiles, rdkit_mol, umbra_mol from molecule where umbra_is_exact_match(umbra_mol,'CCC');
+
+SELECT pbd.prediction_method, a.value, a.relation, m.umbra_mol FROM molecule m
+  INNER JOIN activities a ON a.molregno=m.molregno
+  INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+  INNER JOIN compound_properties cp ON cp.molregno=m.molregno
+  WHERE umbra_is_exact_match(m.umbra_mol, 'COc1cc(/C=C/C(=O)OCCCCCCN(C)CCCCOC(=O)c2c3ccccc3cc3ccccc23)cc(OC)c1OC');
+
+SELECT avg(a.value), count(a.value), a.relation, m.umbra_mol FROM molecule m
+  INNER JOIN activities a ON a.molregno=m.molregno
+  INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+  INNER JOIN compound_properties cp ON cp.molregno=m.molregno
+  WHERE umbra_is_exact_match(m.umbra_mol, 'CC(=O)Nc1nnc(S(N)(=O)=O)s1')
+  GROUP BY m.umbra_mol, a.relation;
+
+
+
+
+
+
+-- normal exact match
+select molregno, canonical_smiles, rdkit_mol, umbra_mol from molecule where is_exact_match(mol,'Cc1cn([C@H]2C[C@H](N=[N+]=[N-])[C@@H](CO)O2)c(=O)[nH]c1=O');
+
+select molregno, canonical_smiles, rdkit_mol, umbra_mol from molecule where is_exact_match(mol,'CCC');
+
+
+
+
+SELECT pbd.prediction_method, a.value, a.relation, m.rdkit_mol FROM molecule m
+    INNER JOIN activities a ON a.molregno=m.molregno
+    INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+    INNER JOIN compound_properties cp ON cp.molregno=m.molregno
+    WHERE is_exact_match(m.rdkit_mol, 'COc1cc(/C=C/C(=O)OCCCCCCN(C)CCCCOC(=O)c2c3ccccc3cc3ccccc23)cc(OC)c1OC');
+
+SELECT avg(a.value), count(a.value), a.relation, m.rdkit_mol FROM molecule m
+INNER JOIN activities a ON a.molregno=m.molregno
+INNER JOIN predicted_binding_domains pbd ON pbd.activity_id=a.activity_id
+INNER JOIN compound_properties cp ON cp.molregno=m.molregno
+WHERE is_exact_match(m.rdkit_mol, 'CC(=O)Nc1nnc(S(N)(=O)=O)s1')
+GROUP BY m.rdkit_mol, a.relation;
+
