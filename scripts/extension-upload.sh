9528b33bdc8bc184bb7953cc413cd9e1a3bed903
bodowd <odowd.bing@gmail.com>
2024-08-12 22:56:22 +0200

    update extension-upload script

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -9,7 +9,7 @@
 # <architecture>        : Architecture target of the extension binary
 # <s3_bucket>           : S3 bucket to upload to
 # <copy_to_latest>      : Set this as the latest version ("true" / "false", default: "false")
-# <copy_to_versioned>   : Set this as a versioned version that will prevent its deletion
+# <copy_to_latest>      : Set this as a versioned version that will prevent its deletion
 
 set -e
 
@@ -74,6 +74,9 @@ fi
 # upload versioned version
 if [[ $7 = 'true' ]]; then
   if [[ $4 == wasm* ]]; then
+    # Old style paths with additional duckdb-wasm
+    aws s3 cp $ext.compressed s3://$5/duckdb-wasm/$1/$2/duckdb-wasm/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
+    # New style paths for duckdb-wasm, more uniforms
     aws s3 cp $ext.compressed s3://$5/$1/$2/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
   else
     aws s3 cp $ext.compressed s3://$5/$1/$2/$3/$4/$1.duckdb_extension.gz --acl public-read
@@ -83,6 +86,9 @@ fi
 # upload to latest version
 if [[ $6 = 'true' ]]; then
   if [[ $4 == wasm* ]]; then
+    # Old style paths with additional duckdb-wasm
+    aws s3 cp $ext.compressed s3://$5/duckdb-wasm/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
+    # New style paths for duckdb-wasm, more uniforms
     aws s3 cp $ext.compressed s3://$5/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
   else
     aws s3 cp $ext.compressed s3://$5/$3/$4/$1.duckdb_extension.gz --acl public-read
