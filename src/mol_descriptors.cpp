7c7785aaa5422707cbfc198d8d381a8e3b26b543
bodowd <odowd.bing@gmail.com>
2024-09-30 17:39:36 +0200

    add mol_tpsa function

--- src/mol_descriptors.cpp
+++ src/mol_descriptors.cpp
@@ -5,6 +5,7 @@
 #include "duckdb/common/types/value.hpp"
 #include "duckdb/common/types/vector.hpp"
 #include "duckdb/common/vector_operations/unary_executor.hpp"
+#include "duckdb/execution/expression_executor_state.hpp"
 #include "mol_formats.hpp"
 #include "types.hpp"
 #include "umbra_mol.hpp"
@@ -25,10 +26,29 @@ void mol_exactmw(DataChunk &args, ExpressionState &state, Vector &result) {
       });
 }
 
+void mol_tpsa(DataChunk &args, ExpressionState &state, Vector &result) {
+  D_ASSERT(args.data.size() == 1);
+  auto &binary_umbra_mol = args.data[0];
+  auto count = args.size();
+
+  UnaryExecutor::Execute<string_t, float>(
+      binary_umbra_mol, result, count, [&](string_t b_umbra_mol) {
+        auto umbra_mol = umbra_mol_t(b_umbra_mol);
+        auto bmol = umbra_mol.GetBinaryMol();
+        auto mol = rdkit_binary_mol_to_mol(bmol);
+        return RDKit::Descriptors::calcTPSA(*mol);
+      });
+}
+
 void RegisterDescriptorFunctions(DatabaseInstance &instance) {
-  ScalarFunctionSet set_exactmw("exactmw");
-  set_exactmw.AddFunction(
-      ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, exactmw));
-  ExtensionUtil::RegisterFunction(instance, set_exactmw);
+  ScalarFunctionSet set_mol_exactmw("mol_exactmw");
+  set_mol_exactmw.AddFunction(
+      ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, mol_exactmw));
+  ExtensionUtil::RegisterFunction(instance, set_mol_exactmw);
+
+  ScalarFunctionSet set_mol_tpsa("mol_tpsa");
+  set_mol_tpsa.AddFunction(
+      ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, mol_tpsa));
+  ExtensionUtil::RegisterFunction(instance, set_mol_tpsa);
 }
 } // namespace duckdb_rdkit
