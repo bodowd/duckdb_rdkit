c3ca73091b6eab018e88d9ad82ce0a9fbe4e9085
bodowd <odowd.bing@gmail.com>
2024-10-03 11:51:21 +0200

    add approximate mol weight descriptor (mol_amw)

--- src/mol_descriptors.cpp
+++ src/mol_descriptors.cpp
@@ -12,6 +12,20 @@
 
 namespace duckdb_rdkit {
 
+void mol_amw(DataChunk &args, ExpressionState &state, Vector &result) {
+  D_ASSERT(args.data.size() == 1);
+  auto &binary_umbra_mol = args.data[0];
+  auto count = args.size();
+
+  UnaryExecutor::Execute<string_t, float>(
+      binary_umbra_mol, result, count, [&](string_t b_umbra_mol) {
+        auto umbra_mol = umbra_mol_t(b_umbra_mol);
+        auto bmol = umbra_mol.GetBinaryMol();
+        auto mol = rdkit_binary_mol_to_mol(bmol);
+        return RDKit::Descriptors::calcAMW(*mol);
+      });
+}
+
 void mol_exactmw(DataChunk &args, ExpressionState &state, Vector &result) {
   D_ASSERT(args.data.size() == 1);
   auto &binary_umbra_mol = args.data[0];
@@ -41,6 +55,11 @@ void mol_tpsa(DataChunk &args, ExpressionState &state, Vector &result) {
 }
 
 void RegisterDescriptorFunctions(DatabaseInstance &instance) {
+  ScalarFunctionSet set_mol_amw("mol_amw");
+  set_mol_amw.AddFunction(
+      ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, mol_amw));
+  ExtensionUtil::RegisterFunction(instance, set_mol_amw);
+
   ScalarFunctionSet set_mol_exactmw("mol_exactmw");
   set_mol_exactmw.AddFunction(
       ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, mol_exactmw));
