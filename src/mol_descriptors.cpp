d9b09f70f298d1fdcfb2e667c023d226015a19ea
bodowd <odowd.bing@gmail.com>
2024-12-10 21:24:07 +0100

    add mol_logp function and test

--- src/mol_descriptors.cpp
+++ src/mol_descriptors.cpp
@@ -1,17 +1,36 @@
 #include "mol_descriptors.hpp"
 #include "common.hpp"
 #include "duckdb/common/assert.hpp"
+#include "duckdb/common/types.hpp"
 #include "duckdb/common/types/data_chunk.hpp"
 #include "duckdb/common/types/value.hpp"
 #include "duckdb/common/types/vector.hpp"
 #include "duckdb/common/vector_operations/unary_executor.hpp"
 #include "duckdb/execution/expression_executor_state.hpp"
+#include "duckdb/function/function_set.hpp"
+#include "duckdb/main/extension_util.hpp"
 #include "mol_formats.hpp"
 #include "qed.hpp"
 #include "types.hpp"
 #include "umbra_mol.hpp"
 
 namespace duckdb_rdkit {
+
+void mol_logp(DataChunk &args, ExpressionState &state, Vector &result) {
+  D_ASSERT(args.data.size() == 1);
+  auto &binary_umbra_mol = args.data[0];
+  auto count = args.size();
+
+  UnaryExecutor::Execute<string_t, float>(
+      binary_umbra_mol, result, count, [&](string_t b_umbra_mol) {
+        auto umbra_mol = umbra_mol_t(b_umbra_mol);
+        auto bmol = umbra_mol.GetBinaryMol();
+        auto mol = rdkit_binary_mol_to_mol(bmol);
+        double logp, _;
+        RDKit::Descriptors::calcCrippenDescriptors(*mol, logp, _);
+        return logp;
+      });
+}
 
 void mol_qed(DataChunk &args, ExpressionState &state, Vector &result) {
   D_ASSERT(args.data.size() == 1);
@@ -90,5 +109,10 @@ void RegisterDescriptorFunctions(DatabaseInstance &instance) {
   set_mol_qed.AddFunction(
       ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, mol_qed));
   ExtensionUtil::RegisterFunction(instance, set_mol_qed);
+
+  ScalarFunctionSet set_mol_logp("mol_logp");
+  set_mol_logp.AddFunction(
+      ScalarFunction({duckdb_rdkit::Mol()}, LogicalType::FLOAT, mol_logp));
+  ExtensionUtil::RegisterFunction(instance, set_mol_logp);
 }
 } // namespace duckdb_rdkit
